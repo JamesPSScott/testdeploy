4747f2024d0014b5342a9397e2fdbe2a706d21ea
James Scott <james.scott@pharm3r.com>
2024-01-12 12:40:40 -0400

    adding a trigger

---
+++ TestAzFunction/TestHttpFunction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace TestAzFunction;
+
+public class TestHttpFunction
+{
+    private readonly ILogger _logger;
+
+    public TestHttpFunction(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<TestHttpFunction>();
+    }
+
+    [Function("TestHttpFunction")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
+        
+    }
+}
