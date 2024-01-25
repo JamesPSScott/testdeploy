82f55737dee9f00fd54addbb7cdcaa36aabac378
James Scott <james.scott@pharm3r.com>
2024-01-25 17:59:49 -0400

    trying again

--- TestAzFunction/Program.cs
+++ TestAzFunction/Program.cs
@@ -12,10 +12,7 @@ var host = new HostBuilder()
         {
             configBuilder.AddAzureKeyVault(
                 new Uri("https://test-key-vault-jpss.vault.azure.net/"),
-                new DefaultAzureCredential(new DefaultAzureCredentialOptions
-                {
-                    ManagedIdentityClientId = "ccba3238-7fe7-4fdb-a30c-9bec1f5dac45"//"e2df078a-ae8a-48ee-a296-5ad8f5e4d99b"//"c901d444-6b59-4ff7-a5b2-6b0fa3f6fe17"
-                }));
+                new DefaultAzureCredential());
         }
 
     })
